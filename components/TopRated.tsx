6d30caebf5363662b206cf6ccf0cb3a959e0137d
NedxLab <odoc047@gmail.com>
2023-02-16 23:12:22 +0100

    extend types and redux

--- components/TopRated.tsx
+++ components/TopRated.tsx
@@ -1,54 +1,58 @@
 import movieData from "./stream";
 import Image from "next/image";
-import { log } from "console";
+import { useSelector, useDispatch } from "react-redux";
 import React, { useState, useEffect } from "react";
 
 const TopRated = () => {
-  var topRatedMovies = [];
-  const [filteredTopRatedMovies, setFilteredTopRatedMovies] = useState([]);
-  const [active, setActive] = useState("");
-  const filterTopRatedMovies = (genre) => {
-    setActive(genre);
-    topRatedMovies = [];
-    var movies = movieData.filter((movie) => movie.genre === genre);
+  const { topRatedMovies } = useSelector((store) => store.topRatedMovies);
+  const dispatch = useDispatch();
+  const imageUrl = "https://image.tmdb.org/t/p/original";
 
-    movies = movies.length < 1 ? movieData : movies;
-    let topRated = movies.map((movie, index) => {
-      return parseFloat(movie.rating);
-    });
-    topRated = topRated.sort(function (a, b) {
-      return b - a;
-    });
+  // var topRatedMovies = [];
+  // const [filteredTopRatedMovies, setFilteredTopRatedMovies] = useState([]);
+  // const [active, setActive] = useState("");
+  // const filterTopRatedMovies = (genre) => {
+  //   setActive(genre);
+  //   topRatedMovies = [];
+  //   var movies = movieData.filter((movie) => movie.genre === genre);
 
-    topRated = topRated.slice(0, 4);
-    // console.log(topRated);
+  //   movies = movies.length < 1 ? movieData : movies;
+  //   let topRated = movies.map((movie, index) => {
+  //     return parseFloat(movie.rating);
+  //   });
+  //   topRated = topRated.sort(function (a, b) {
+  //     return b - a;
+  //   });
 
-    var filteredMovies = topRated.map((newmovie) => {
-      // console.log(newmovie);
-      return movies.filter((movie) => parseFloat(movie.rating) === newmovie);
-    });
+  //   topRated = topRated.slice(0, 4);
+  //   // console.log(topRated);
 
-    filteredMovies.map((movie) => {
-      if (Array.isArray(movie)) {
-        movie.map((movie) => {
-          topRatedMovies.push(movie);
-        });
-      } else {
-        topRatedMovies.push(movie);
-      }
-    });
-    // console.log([...new Set(topRatedMovies)]);
-    topRatedMovies = [...new Set(topRatedMovies)];
-    // console.log(topRatedMovies);
+  //   var filteredMovies = topRated.map((newmovie) => {
+  //     // console.log(newmovie);
+  //     return movies.filter((movie) => parseFloat(movie.rating) === newmovie);
+  //   });
 
-    // return (
-    topRatedMovies = topRatedMovies.slice(0, 4);
-    setFilteredTopRatedMovies(topRatedMovies);
-    // );
-  };
-  useEffect(() => {
-    filterTopRatedMovies("all");
-  }, []);
+  //   filteredMovies.map((movie) => {
+  //     if (Array.isArray(movie)) {
+  //       movie.map((movie) => {
+  //         topRatedMovies.push(movie);
+  //       });
+  //     } else {
+  //       topRatedMovies.push(movie);
+  //     }
+  //   });
+  //   // console.log([...new Set(topRatedMovies)]);
+  //   topRatedMovies = [...new Set(topRatedMovies)];
+  //   // console.log(topRatedMovies);
+
+  //   // return (
+  //   topRatedMovies = topRatedMovies.slice(0, 4);
+  //   setFilteredTopRatedMovies(topRatedMovies);
+  //   // );
+  // // };
+  // useEffect(() => {
+  //   filterTopRatedMovies("all");
+  // }, []);
 
   return (
     <section className="top-rated-movie tr-movie-bg">
@@ -64,37 +68,15 @@ const TopRated = () => {
         <div className="row justify-content-center">
           <div className="col-lg-8">
             <div className="tr-movie-menu-active text-center">
-              <button
-                className={active === "all" ? "active" : ""}
-                onClick={() => filterTopRatedMovies("all")}
-              >
-                All
-              </button>
-              <button
-                className={active === "series" ? "active" : ""}
-                onClick={() => {
-                  filterTopRatedMovies("series");
-                }}
-              >
-                Series
-              </button>
-              <button
-                className={active === "action" ? "active" : ""}
-                onClick={() => filterTopRatedMovies("action")}
-              >
-                Action
-              </button>
-              <button
-                className={active === "shows" ? "active" : ""}
-                onClick={() => filterTopRatedMovies("shows")}
-              >
-                shows
-              </button>
+              <button className={"active"}>All</button>
+              <button className={""}>Series</button>
+              <button className={""}>Action</button>
+              <button className={""}>shows</button>
             </div>
           </div>
         </div>
         <div className="row tr-movie-active">
-          {filteredTopRatedMovies.map((movie, i) => (
+          {topRatedMovies.map((movie: string[], i: number) => (
             <div
               key={i}
               className="col-xl-3 col-lg-4 col-sm-6 grid-item grid-sizer cat-two"
@@ -103,7 +85,7 @@ const TopRated = () => {
                 <div className="movie-poster">
                   <a href="movie-details.html">
                     <Image
-                      src={movie.image}
+                      src={imageUrl + movie.poster_path}
                       alt="image"
                       className="rounded-2xl h-96"
                     />
@@ -112,21 +94,24 @@ const TopRated = () => {
                 <div className="movie-content">
                   <div className="top">
                     <h5 className="title">
-                      <a href="movie-details.html">{movie.title}</a>
+                      <a href="movie-details.html">
+                        {movie.original_title || movie.original_name}
+                      </a>
                     </h5>
-                    <span className="date">{movie.year}</span>
+                    <span className="date">{movie.release_date}</span>
                   </div>
                   <div className="bottom">
                     <ul>
-                      <li>
+                      {/* <li>
                         <span className="quality">{movie.resolution}</span>
-                      </li>
+                      </li> */}
                       <li>
-                        <span className="duration">
+                        {/* <span className="duration">
                           <i className="far fa-clock"></i> {movie.duration}
-                        </span>
+                        </span> */}
                         <span className="rating">
-                          <i className="fas fa-thumbs-up"></i> {movie.rating}
+                          <i className="fas fa-thumbs-up"></i>{" "}
+                          {movie.vote_average}
                         </span>
                       </li>
                     </ul>
